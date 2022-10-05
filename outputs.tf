073f0add81eafefc6361b0917151855e0844af30
mamos88 <go_mamos@yahoo.com>
2022-10-05 09:22:51 -0700

    Fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "private_ip_addr" {
-  value = aws_instance.nomad-node[*].private_ip
+  value = aws_instance.nomad-server-node[*].private_ip
 }
 
 output "public_ip_addr" {
-  value = aws_instance.nomad-node[*].public_ip
+  value = aws_instance.nomad-server-node[*].public_ip
 }
 
 output "consul_url" {
-  value = "http://${aws_instance.nomad-node[0].public_ip}:8500/ui"
+  value = "http://${aws_instance.nomad-server-node[0].public_ip}:8500/ui"
 }
 
 output "nomad_url" {
-  value = "http://${aws_instance.nomad-node[0].public_ip}:4646/ui"
+  value = "http://${aws_instance.nomad-server-node[0].public_ip}:4646/ui"
 }
 
 output "ec2_nodes" {
-  value = aws_instance.nomad-node[*].public_dns
+  value = aws_instance.nomad-server-node[*].public_dns
 }
