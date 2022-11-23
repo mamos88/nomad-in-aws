1d0b54b6fa0d56a36b57f23a98f8beef638222ac
mamos88 <go_mamos@yahoo.com>
2022-11-23 12:46:41 -0700

    Adding Load Balancers

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,44 +1,20 @@
-resource "aws_lb_target_group" "alb_target_group" {
-  name     = "test-target-group"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.nomad-lab-vpc.id
-  target_type = "instance"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/24"
-}
-
-resource "aws_lb" "alb" {
+resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.nomad-sg.id]
-  subnets            = [for subnet in aws_subnet.nomad-lab-pub : subnet.id]
+  subnets            = [aws_subnet.nomad-lab-pub]
 
   enable_deletion_protection = true
+
+#   access_logs {
+#     bucket  = "${aws_s3_bucket.lb_logs.bucket}"
+#     prefix  = "test-lb"
+#     enabled = true
+#   }
 
   tags = {
     Environment = "production"
   }
-  depends_on = [
-    aws_subnet.nomad-lab-pub
-  ]
 }
-
-resource "aws_alb_listener" "alb_listener" {  
-  load_balancer_arn = "${aws_alb.alb.arn}"  
-  port              = "${var.alb_listener_port}"  
-  protocol          = "${var.alb_listener_protocol}"
-  
-  default_action {    
-    target_group_arn = "${aws_alb_target_group.alb_target_group.arn}"
-    type             = "forward"  
-  }
-  depends_on = [
-    aws_vpc.nomad-lab-vpc
-  ]
-}
-
 
