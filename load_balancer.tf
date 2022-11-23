260a3d4016e5e802ae647755627b8df063e0ec18
mamos88 <go_mamos@yahoo.com>
2022-11-23 14:11:15 -0700

    Adding Load Balancers

--- load_balancer.tf
+++ load_balancer.tf
@@ -18,3 +18,22 @@ resource "aws_lb" "test" {
   }
 }
 
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_lb.test.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.test.arn}"
+  }
+}
+
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.nomad-lab-vpc.id}"
+}
