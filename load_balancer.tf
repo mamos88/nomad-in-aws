9cd1b73c5ef91b3939504d36dc944f7714f9a7d6
mamos88 <go_mamos@yahoo.com>
2022-11-23 14:26:38 -0700

    Adding Load Balancers

--- load_balancer.tf
+++ load_balancer.tf
@@ -37,3 +37,10 @@ resource "aws_lb_target_group" "test" {
   protocol = "HTTP"
   vpc_id   = "${aws_vpc.nomad-lab-vpc.id}"
 }
+
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = "${aws_lb_target_group.test.arn}"
+  target_id        = "${aws_instance.amazon-client-nodes.id}"
+  port             = 80
+}
