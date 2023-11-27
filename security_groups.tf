4c00ab983a17d8c784ea9f1e2d1c07201b6ab0be
mamos88 <go_mamos@yahoo.com>
2023-11-27 15:41:42 -0700

    Working dynamic block

--- security_groups.tf
+++ security_groups.tf
@@ -105,7 +105,66 @@ variable "nomad_ingress" {
   }
 }
 
+variable "nomad_egress" {
+  type = map(object({
+    description = string
+    to_port = number
+    from_port = number
+    protocol = string
+    cidr_blocks = list(string)
+  }))
+  default = {
+    "0-65535-tcp" = {
+      description = "Egress Rules TCP"
+      to_port = 65535
+      from_port = 0
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    "0-65535-udp" = {
+      description = "Egress Rules UDP"
+      to_port = 65535
+      from_port = 0
+      protocol = "udp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
 
+variable "application_ingress" {
+  type = map(object({
+    description = string
+    port = number
+    protocol = string
+    cidr_blocks = list(string)
+  }))
+  default = {
+    "9090" = {
+      description = "Prometheus"
+      port = 9090
+      protocol = "tcp"
+      cidr_blocks = ["174.26.254.31/32", "10.0.0.0/16"]
+    }
+    "3000" = {
+      description = "Grafana"
+      port = 3000
+      protocol = "tcp"
+      cidr_blocks = ["174.26.254.31/32", "10.0.0.0/16"]
+    }
+    "9200" = {
+      description = "Elasticsearch"
+      port = 9200
+      protocol = "tcp"
+      cidr_blocks = ["174.26.254.31/32", "10.0.0.0/16"]
+    }
+    "5601" = {
+      description = "Kibana"
+      port = 5601
+      protocol = "tcp"
+      cidr_blocks = ["174.26.254.31/32", "10.0.0.0/16"]
+    }
+  }
+}
 
 
 resource "aws_security_group" "nomad-sg" {
@@ -121,212 +180,37 @@ resource "aws_security_group" "nomad-sg" {
       protocol = ingress.value.protocol
       cidr_blocks = ingress.value.cidr_blocks      
     }
-    
   }
 
-/*
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
+  dynamic "ingress" {
+    for_each = var.application_ingress
+    content {
+      description = ingress.value.description
+      from_port = ingress.value.port
+      to_port = ingress.value.port
+      protocol = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks      
+    }
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
+    dynamic "egress" {
+    for_each = var.nomad_egress
+    content {
+      description = egress.value.description
+      from_port = egress.value.from_port
+      to_port = egress.value.to_port
+      protocol = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks      
+    }
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 1936
-    to_port     = 1936
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 4646
-    to_port     = 4646
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 4647
-    to_port     = 4647
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 4648
-    to_port     = 4648
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 4648
-    to_port     = 4648
-    protocol    = "udp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  # This is needed for haproxy based on the port I chose
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8300
-    to_port     = 8300
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
- # Vault Ports
-  ingress {
-    from_port   = 8200
-    to_port     = 8200
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8201
-    to_port     = 8201
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8301
-    to_port     = 8301
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8301
-    to_port     = 8301
-    protocol    = "udp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8302
-    to_port     = 8302
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8302
-    to_port     = 8302
-    protocol    = "udp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-
-  ingress {
-    from_port   = 8400
-    to_port     = 8400
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8500
-    to_port     = 8500
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8600
-    to_port     = 8600
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 8600
-    to_port     = 8600
-    protocol    = "udp"
-    cidr_blocks = var.allowed_ip_network
-  }
-*/
+# Consul Sidecar port range
   ingress {
     from_port   = 21000
-    to_port     = 21000
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-  ingress {
-    from_port   = 21255
     to_port     = 21255
     protocol    = "tcp"
     cidr_blocks = var.allowed_ip_network
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-    egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # # Prometheus
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-  # Grafana
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-# Elasticsearch
-  ingress {
-    from_port   = 9200
-    to_port     = 9200
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
-# Kibana
-  ingress {
-    from_port   = 5601
-    to_port     = 5601
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ip_network
-  }
-
 
   ingress {
     from_port   = 0
